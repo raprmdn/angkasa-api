51462285a091a23ae83b1bbf74f9cee0fd3840bc
lilikwn <lilik1.wahyu7@gmail.com>
2022-11-25 15:09:39 +0700

    add feat:google-callback register/login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const googleLogin = require('./google.route')
 
 const router = express.Router();
 
 // router.use()
+router.use('/login/google', googleLogin);
 
 module.exports = router;
