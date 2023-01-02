e0de08a69b5c9775abcfb97f81d4e09fa4d07a00
raprmdn <rafiputraramadhan14@gmail.com>
2023-01-02 13:04:52 +0700

    Fix flights scheduler.

--- src/utils/scheduler.utils.js
+++ src/utils/scheduler.utils.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const { Flight, SeatPrice } = require('../models');
 const { Op } = require('sequelize');
 const moment = require('moment');
-const { SeedFlights, SeedFlightSeatPrices} = require("../helpers/flight.helper");
+const { SeedFlights, SeedFlightSeatPrices } = require("../helpers/flight.helper");
 
 // Run Every 1 Hour
 cron.schedule('0 * * * *', async () => {
@@ -39,14 +39,20 @@ cron.schedule('0 * * * *', async () => {
     `);
 });
 
-// Run every 1 week
-cron.schedule('0 0 * * 0', async () => {
-    const flights = SeedFlights();
-    await Flight.bulkCreate(flights).then(async (flights) => {
-        const seatPrices = SeedFlightSeatPrices(flights);
-        await SeatPrice.bulkCreate(seatPrices);
-        console.info(`
-        Scheduler: (Flights Scheduler) [Create new flights every 1 week]
+// Run every day at midnight
+cron.schedule('0 0 * * *', async () => {
+    const maxDate = await Flight.max('date');
+    const maxDateMoment = moment(maxDate).format('YYYY MM DD HH:mm:ss');
+    const now = moment().format('YYYY MM DD HH:mm:ss');
+
+    if (now > maxDateMoment) {
+        console.info(`There's no more flights scheduled. New flights will be created.`);
+        const flights = SeedFlights();
+        await Flight.bulkCreate(flights).then(async (flights) => {
+            const seatPrices = SeedFlightSeatPrices(flights);
+            await SeatPrice.bulkCreate(seatPrices);
+            console.info(`
+        Scheduler: (Flights Scheduler) [Create new flights for the next 7 days]
         Flights created: ${flights.length}
         Flights:
         ${JSON.stringify(flights, null, 2)}
@@ -55,5 +61,16 @@ cron.schedule('0 0 * * 0', async () => {
         Type: CREATE_FLIGHTS
         Triggered at: ${moment().format('MMMM DD, YYYY HH:mm:ss')}
         `);
-    });
+        });
+    }
+
+    console.info(`
+    Scheduler: (Flights Scheduler) [Check if there's no more flights, if so, create new flights schedule]
+    Runner: Running every 1 day at 01:00
+    Max Date: ${maxDateMoment}
+    Now: ${now}
+    Type: CHECK_FLIGHTS
+    Triggered at: ${moment().format('MMMM DD, YYYY HH:mm:ss')}
+    Status: ${now >= maxDateMoment ? 'No more flights scheduled. New flights schedule has been created.' : 'There are still flights scheduled'}
+    `)
 });
